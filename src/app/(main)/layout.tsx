b870a02cd1308065711b0e50e294f7b14eba5532
anasnadkar45 <anasnadkar23@gmail.com>
2024-10-13 09:06:16 +0530

    Project Initialized

---
+++ src/app/(main)/layout.tsx
@@ -0,0 +1,16 @@
+import { Sidebar } from "../components/global/Sidebar";
+
+export default async function ProjectLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <div className="w-screen h-screen flex custom-scrollbar scroll-smooth">
+            <Sidebar />
+            <div className="m-2 rounded-md border w-full bg-secondary">
+                {children}
+            </div>
+        </div>
+    );
+}
---
+++ src/app/components/global/Sidebar.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Sidebar = () => {
+  return (
+    <div className='h-screen w-[200px] p-2'>Sidebar</div>
+  )
+}
