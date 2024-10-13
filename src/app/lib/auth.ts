09e2ef414bfcbaa191277acd9424848ef600eb5d
anasnadkar45 <anasnadkar23@gmail.com>
2024-10-13 10:43:43 +0530

    Add .env file

---
+++ src/app/lib/auth.ts
@@ -0,0 +1,11 @@
+import NextAuth from "next-auth"
+import Github from 'next-auth/providers/github'
+import Google from "next-auth/providers/google"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import prisma from "./db"
+
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+    adapter: PrismaAdapter(prisma),
+    providers: [Github, Google],
+})
